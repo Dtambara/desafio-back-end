7cba9bb12cc9f979d7d65173198e4b1af9c99802
dtambara <danilo.tambara.2011@gmail.com>
2018-10-09 17:53:10 -0300

    Arquivo de rotas

---
+++ app/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const requireDir = require("require-dir");
+
+const routes = express.Router();
+const controllers = requireDir("./controllers");
+
+module.exports = routes;
