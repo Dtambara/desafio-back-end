8c8543b619995333dd8b23cfce6fb9966d96a652
dtambara <danilo.tambara.2011@gmail.com>
2018-10-10 14:45:55 -0300

    Adicionando cadastro e login de usuários

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,10 @@ const requireDir = require("require-dir");
 const routes = express.Router();
 const controllers = requireDir("./controllers");
 
+/**
+ * Auth
+ */
+routes.post("/signup", controllers.authController.signup);
+routes.post("/signin", controllers.authController.signin);
+
 module.exports = routes;
