87f5ab13863a2b64f3df0feb0d56be6c5fe952ed
dtambara <danilo.tambara@al.forsoft.org.br>
2018-10-15 23:12:44 -0300

    Alterações no método que retorna o feed

--- app/controllers/feedController.js
+++ app/controllers/feedController.js
@@ -3,48 +3,65 @@ const himalaya = require('himalaya');
 
 const parser = new Parser();
 
-function procuraLinksNosElementos(element, ...arrayLinks) {
+function procuraLinksNosElementos(element) {
   element.children.map((child) => {
     if (child.tagName === 'li') {
       procuraLinksNosElementos(child);
     } else if (child.tagName === 'a') {
       child.attributes.map((attribute) => {
         if (attribute.key === 'href') {
+          const arrayLinks = [];
           arrayLinks.push(attribute.value);
+          return arrayLinks;
         }
       });
     }
   });
+  return [];
+}
+
+function montaDescricaoTexto(item, child) {
+  const descriptionObjectText = {};
+  descriptionObjectText.type = 'text';
+  descriptionObjectText.content = child.children[0].content;
+  item.description.push(descriptionObjectText);
+}
+
+function montaDescricaoImagem(element, item) {
+  const descriptionObjectDiv = {};
+  descriptionObjectDiv.type = 'image';
+  element.attributes.map((attribute) => {
+    if (attribute.key === 'src') {
+      descriptionObjectDiv.content = attribute.value;
+    }
+  });
+  item.description.push(descriptionObjectDiv);
+}
+
+function montaDescricaoLinks(element, item) {
+  const descriptionObjectDiv = {};
+  descriptionObjectDiv.type = 'links';
+  descriptionObjectDiv.content = procuraLinksNosElementos(element);
+  item.description.push(descriptionObjectDiv);
+}
+
+function formataPeloTipoDescricao(item, child) {
+  if (child.tagName === 'p') {
+    montaDescricaoTexto(item, child);
+  } else if (child.tagName === 'div') {
+    child.children.map((element) => {
+      if (element.tagName === 'img') {
+        montaDescricaoImagem(element, item);
+      } else if (element.tagName === 'ul') {
+        montaDescricaoLinks(element, item);
+      }
+    });
+  }
 }
 
 function formataDescription(itemResponseDescriptionJson, item) {
   itemResponseDescriptionJson.map((child) => {
-    if (child.tagName === 'p') {
-      const descriptionObjectText = {};
-      descriptionObjectText.type = 'text';
-      descriptionObjectText.content = child.children[0].content;
-      item.description.push(descriptionObjectText);
-    } else if (child.tagName === 'div') {
-      child.children.map((element) => {
-        if (element.tagName === 'img') {
-          const descriptionObjectDiv = {};
-          descriptionObjectDiv.type = 'image';
-          element.attributes.map((attribute) => {
-            if (attribute.key === 'src') {
-              descriptionObjectDiv.content = attribute.value;
-            }
-          });
-          item.description.push(descriptionObjectDiv);
-        } else if (element.tagName === 'ul') {
-          const descriptionObjectDiv = {};
-          descriptionObjectDiv.type = 'links';
-          descriptionObjectDiv.content = [];
-          procuraLinksNosElementos(element, descriptionObjectDiv.content);
-          console.log(descriptionObjectDiv.content);
-          item.description.push(descriptionObjectDiv);
-        }
-      });
-    }
+    formataPeloTipoDescricao(item, child);
   });
 }
 
